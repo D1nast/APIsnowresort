66c83b235d989f07175c1ea4ea39b9d5af6f17a4
Ishihara <almond1024r@gmail.com>
2023-03-18 16:55:58 +0900

    as

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "https://snowresort-search.com"
+      origins "https://snowresort-search.com", "https://snowresort-search.com", "http://localhost:3001/"
   
       resource '*',
           headers: :any,
